348a9e22bd879d524d3601c68412556fe4080dd8
dangngocthanh <helomilo123@gmail.com>
2021-09-14 10:11:45 +0700

    test

---
+++ cau4/main.js
@@ -0,0 +1,14 @@
+let objAnimal1 = new Animal('Elephant', 45.6)
+objAnimal1.toString()
+let objAnimal2 = new Animal('Cat', 15)
+objAnimal2.setName('Mouse')
+/*Em có chút không hiểu về đề:
+Thứ nhất là ban đầu đề yêu cầu tạo đối tượng có tên và objAnimal1
+rồi sau đó yêu cầu thiết đặt tên và cân nặng. sau đó gọi toString
+tạo đối tượng objAnimal2 lại nói là sử dụng phương thức khởi tạo
+có hai tham số là tên và cân nặng. Em thấy yêu cầu cuối cùng là
+đổi tên cho đối tượng 2 nên em nghĩ là yêu cầu 2 và 3 là thiết
+đặt đó vẫn là một phần của khởi tạo. Và cáy 4 yêu cầu dùng phương
+thức khởi tạo có 2 tham số tên và cân nặng mà không ghi cụ thể nên
+em tự đặt tên và cân năngj luôn
+ */
